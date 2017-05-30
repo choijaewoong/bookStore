c65409d705679ea71e269308b3d4877b388d9a5d
choijaewoong <jwchoi0129@gmail.com>
2017-05-30 13:54:41 +0900

    index 페이지에 delete book 기능 추가

--- assets/js/view/viewIndex.js
+++ assets/js/view/viewIndex.js
@@ -14,24 +14,20 @@ pl.view.index = {
             bookList.appendChild(listItem);
         }
 
-        // create add item form
+        // create form and submit button for add
         var addButton = document.getElementById('btn-add-book');
         addButton.addEventListener('click',
             pl.view.index.createInputForm);
 
-        // create radio button for update book
+        // create radio button and submit button for update
         var updateButton = document.getElementById('btnUpdateBook');
         updateButton.addEventListener('click', 
             function() { pl.view.index.createInputRadio(keys)});
-
+        
+        // create select box and submit button for button
         var deleteButton = document.getElementById('btnDeleteBook');
         deleteButton.addEventListener('click',
             function() { pl.view.index.createDeleteCheckbox(keys)});
-    },
-    templateListItem : function(isbn, title, year) {
-        return '<span class="field-isbn">' + isbn + '</span>\
-                <span class="field-title">' + title + '</span>\
-                <span class="field-year">' + year + '</span>';
     },
     handleSaveButtonClickEvent: function() {
         var itemform = document.getElementById('formAddItem');
@@ -52,6 +48,18 @@ pl.view.index = {
         optionEl.text = slots.title;
         optionEl.year = slots.year;
         formEl.reset();
+    },
+    handleDeleteButtonClickEvent: function() {
+        var selectBox = document.querySelectorAll('.input-radio');
+        var slotsList;
+
+        Array.prototype.forEach.call(radios, function(radio) {
+            radio.addEventListener('change', function() {
+                book = Book.instances[this.value];                
+                pl.view.index.textUpdateForm(book.isbn, book.title, book.year);
+            });
+        });
+
     },
     createInputForm: function() {
         if(document.getElementById('formAddItem') !== null) return;
@@ -119,21 +127,19 @@ pl.view.index = {
             radio.type = "radio";
             radio.name = "book";
             radio.value = book.isbn;
-            form.appendChild(radio);
-        }
-        pl.view.index.createUpdateForm();
-        var radios = document.querySelectorAll('.input-radio');
-        Array.prototype.forEach.call(radios, function(radio) {
             radio.addEventListener('change', function() {
                 book = Book.instances[this.value];                
                 pl.view.index.textUpdateForm(book.isbn, book.title, book.year);
             });
-        });
+            form.appendChild(radio);
+        }
+        pl.view.index.createUpdateForm();
     },
     createDeleteCheckbox: function(keys) {
         if(document.querySelector('.form-delete') !== null) return;
         var container = document.querySelector('.contents-main');
         var form = document.createElement('form');
+        var bookArr = [];
         form.className = 'form-delete';
         container.appendChild(form);
         for(i=0; i<keys.length; i++){
@@ -144,21 +150,44 @@ pl.view.index = {
             checkbox.type = "checkbox";
             checkbox.name = "book";
             checkbox.value = book.isbn;
+            checkbox.addEventListener('change', function() {
+                if(this.checked) {
+                    bookArr.push(this.value);
+                    console.log(bookArr);
+                } else {
+                    bookArr.splice(bookArr.indexOf(this.value),1);
+                    console.log(bookArr);
+                }
+            })
             form.appendChild(checkbox);
         }
-        pl.view.index.createDeleteConfirmButton();
+        pl.view.index.createDeleteConfirmButton(bookArr);
     },
-    createDeleteConfirmButton: function() {
+    createDeleteConfirmButton: function(bookArr) {
         var bookList = document.querySelector('.list-book');
+        var deleteForm = document.querySelector('.form-delete');
         var deleteConfirmButton = document.createElement('li');
         deleteConfirmButton.className = 'list-item-book';
         deleteConfirmButton.innerHTML = pl.view.index.templateDeleteConfirmButton();
         bookList.appendChild(deleteConfirmButton);
         var cancelButton = document.getElementById('btnCancel');
         cancelButton.addEventListener('click', function() {
-            this.parentElement.parentElement.remove();
-            var radioForm = document.querySelector('.form-radio');
-            radioForm.remove();
+            deleteConfirmButton.remove();
+            deleteForm.remove();
+        });
+        var submitButton = document.getElementById('btnDeleteItem');
+        submitButton.addEventListener('click', function() {
+            for(var i=0; i<bookArr.length; i++) {
+                var deleteElement = document.querySelector(".field-isbn[data-isbn='" + bookArr[i] + "']").parentElement;
+                console.log(deleteElement);
+                deleteElement.remove();
+                deleteConfirmButton.remove();
+                deleteForm.remove();
+                Book.destroy(bookArr[i]);
+            }
+        });
+        window.addEventListener("beforeunload", function() {
+            Book.saveAll();
         });
     },
     textUpdateForm: function(isbn, title, year) {
@@ -168,10 +197,11 @@ pl.view.index = {
         isbnInput.value = isbn;
         titleInput.value = title;
         yearInput.value = year;
-        console.log(title);
     },
-    clearForm: function() {
-
+    templateListItem : function(isbn, title, year) {
+        return '<span class="field-isbn" data-isbn="' + isbn + '">' + isbn + '</span>\
+                <span class="field-title">' + title + '</span>\
+                <span class="field-year">' + year + '</span>';
     },
     templateAddForm : function() {
         return '<form id="formAddItem" class="form-add-item">\
@@ -190,6 +220,9 @@ pl.view.index = {
                     <button id="btnCancel" class="btn-red btn-submit" type="submit">cancel</button>\
                     <button id="btnUpdateItem" class="btn-brown btn-submit" type="submit">update</button>\
                 </form>';
+    },
+    templateDeleteCheckBox: function(isbn) {
+        return '<input class="input-check" type="checkbox" name="book" value="' + isbn + '">';
     },
     templateDeleteConfirmButton: function() {
         return '<button id="btnCancel" class="btn-red btn-submit" type="submit">cancel</button>\
